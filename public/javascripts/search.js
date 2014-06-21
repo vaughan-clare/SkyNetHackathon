d0525d05ad5aa0a6efd77da62578d42e06f0c073
Joe Raetano <joseph.raetano@gmail.com>
2014-06-21 13:55:04 -0400

    updated search.js with the tracks function parser

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -21,5 +21,27 @@ function search() {
   });
 }
 
+* Store the proper data to tracks and artists list 
+*/
+void onDataLoaded(responseText)
+{
+  JsonObject data = new JsonObject.fromJsonString(responseText);
+  print(data.length);
+  data = data.toptracks;
+  
+  for (var x = 0; x < data.track.length; x++)
+  {
+    //print("Track Name: "+ data.track[x].name.toString());
+    String track = data.track[x].name.toString().trim();
+    //print("Artist: "+data.track[x].artist.name.toString());
+    String artist = data.track[x].artist.name.toString().trim(); 
+    Track_Obj current = new Track_Obj(); 
+    current.Artist = artist;
+    current.Track = track; 
+    //Add the Current Track to the List
+    tracks_list.add(current); 
+  }
+  
+
 
 /* Use this string to construct link: https://www.youtube.com/watch?v=48HPKLZ5b2w" Funny Cat Videos Ever 2014 Funny Cat Videos 2014 Funny ...","thumbnails":{"default":{"url":"https://i.ytimg.com/vi/48HPKLZ5b2w/default.jpg"},"medium":{"url":"https://i.ytimg.com/vi/48HPKLZ5b2w/mqdefault.jpg"},"high":{"url":"https://i.ytimg.com/vi/48HPKLZ5b2w/hqdefault.jpg"}} */
