6eb3867fb03304d6a051dd59f0f30c08e92d4cc5
Asaeli Junior Matelau <matelau@gmail.com>
2014-06-21 12:39:24 -0600

    build google links from query

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -4,7 +4,7 @@ function handleAPILoaded() {
 }
 
 // Search for a specified string.
-function search() {
+function youtubeSearch() {
   var q = $('#query').val();
   var request = gapi.client.youtube.search.list({
     q: q,
@@ -21,40 +21,24 @@ function search() {
   });
 }
 
-* Store the proper data to tracks and artists list 
-*/
-void onDataLoaded(responseText)
-{
-  JsonObject data = new JsonObject.fromJsonString(responseText);
-  print(data.length);
-  data = data.toptracks;
-  
-  for (var x = 0; x < data.track.length; x++)
-  {
-    //print("Track Name: "+ data.track[x].name.toString());
-    String track = data.track[x].name.toString().trim();
-    //print("Artist: "+data.track[x].artist.name.toString());
-    String artist = data.track[x].artist.name.toString().trim(); 
-    Track_Obj current = new Track_Obj(); 
-    current.Artist = artist;
-    current.Track = track; 
-    //Add the Current Track to the List
-    tracks_list.add(current); 
-  }
-  
-
-
 //Build a Search String for third party sites such as coursera, udacity, mit, etc...
-
-function googleSearch(query, site){
+function googleSearch(query){
   // var query = "mobile+development"
-  // var site = 'http%3A%2F%2Fwww.coursera.org%2F'
+  var site1 = 'http%3A%2F%2Fwww.coursera.org%2F'
+  var resource1 = 'coursera';
+  var site2 = 'https%3A%2F%2Fwww.udacity.com%2F'
+  var resource2 = 'udacity';
+
   // example - https://www.google.com/search?as_q=Mobile+development+beginner+java&as_sitesearch=http%3A%2F%2Fwww.coursera.org%2F
-  var link = 'https://www.google.com/search?as_q=';
-  link = link + query + '&as_sitesearch='+ site;
-  // link.concat('&as_sitesearch=');
-  // link.concat(site);
-  $('href='+link).appendTo(".google-search");
+  var glink = 'https://www.google.com/search?as_q=' + query;
+  var link1 = glink + '&as_sitesearch='+ site1;
+  var link2 = glink + '&as_sitesearch='+ site2;
+
+  //update page
+  document.getElementById(resource1).href = link1;
+  document.getElementById(resource1).innerHTML = "Coursera Resources";
+  document.getElementById(resource2).href = link2;
+  document.getElementById(resource2).innerHTML = "Udacity Resources";
 }
 
 /* Use this string to construct link: https://www.youtube.com/watch?v=48HPKLZ5b2w" Funny Cat Videos Ever 2014 Funny Cat Videos 2014 Funny ...","thumbnails":{"default":{"url":"https://i.ytimg.com/vi/48HPKLZ5b2w/default.jpg"},"medium":{"url":"https://i.ytimg.com/vi/48HPKLZ5b2w/mqdefault.jpg"},"high":{"url":"https://i.ytimg.com/vi/48HPKLZ5b2w/hqdefault.jpg"}} */
