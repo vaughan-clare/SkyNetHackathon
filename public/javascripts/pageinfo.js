1cde4d26a704b3f462b868ede33011baca0e3dc2
Vaughan Nickerson <vcnickerson@gmail.com>
2014-06-21 11:43:59 -0700

    Add pageinfo.js

---
+++ public/javascripts/pageinfo.js
@@ -0,0 +1,63 @@
+var startPage = {
+	title: "Choose your new path",
+	queryString: '',
+	opts : [
+		{title: 'Development', desc: 'Are you interested in creating applications?'},
+		{title: 'Intro to CS', desc: 'Are you new to CS?'},
+		{title: 'Sectors', desc: 'Are you interested in learning about the real-life applications of CS?'}
+	]
+};
+var developmentPage = {
+	title:"Development",
+	queryString: 'development',
+	opts: [
+		{title: 'Mobile', desc: 'Are you interested in developing mobile apps?'},
+		{title: 'Web Apps', desc: 'Are you interested in developing apps for the web?'}
+	]
+};
+var introPage = {
+	title:"Intro to CS",
+	queryString: 'beginner cs',
+	opts: [
+		{title: 'Algorithms', desc: ''},
+		{title: 'Data Structures', desc: ''}
+	]
+}
+var sectorsPage = {
+	title:"Sectors",
+	queryString: "'computer science' careers finance automotive energy entertainment medical security",
+	opts: [
+	]
+}
+var mobileLanguagesPage = {
+	title:"Mobile",
+	queryString: "computer language",
+	opts: [
+		{title: 'PHP', desc:'', childPage: 'Level', queryString: "php"},
+		{title: 'Java', desc:'', childPage: 'Level', queryString: "java"},
+		{title: 'Go', desc:'', childPage: 'Level', queryString: "go"},
+		{title: 'Python', desc:'', childPage: 'Level', queryString: "python"}
+	]
+}
+var webLanguagesPage = {
+	title:"Web Apps",
+	queryString: "computer language",
+	opts: [
+		{title: 'PHP', desc:'', childPage: 'Level', queryString: "php"},
+		{title: 'Java', desc:'', childPage: 'Level', queryString: "java"},
+		{title: 'Go', desc:'', childPage: 'Level', queryString: "go"},
+		{title: 'Python', desc:'', childPage: 'Level', queryString: "python"}
+	]
+}
+
+var levelPage = {
+	title: "Level",
+	queryString: "level",
+	opts: [
+		{title: 'Beginner', desc:'Are you just starting out with this language?'},
+		{title: 'Intermediate', desc:'Have you used this language a little?'},
+		{title: 'Advanced', desc:'Are you highly familiar with this language?'}
+	]
+}
+
+var pageArray = [startPage, developmentPage,introPage,sectorsPage,mobileLanguagesPage,webLanguagesPage,levelPage];
