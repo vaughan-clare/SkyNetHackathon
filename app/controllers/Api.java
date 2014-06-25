7cf427d13aa0f0f55d7b9ff4c212c9a9befd04d9
Junior Matelau <matelau@gmail.com>
2014-06-25 16:57:40 -0600

    preparing for creating sprout models

--- app/controllers/Api.java
+++ app/controllers/Api.java
@@ -15,11 +15,12 @@
 
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.*;
 import play.mvc.*;
 import views.html.*;
-//import play.libs.Json;
-//import com.fasterxml.jackson.databind.JsonNode;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class Api extends Controller {
 
@@ -29,9 +30,21 @@ public class Api extends Controller {
 	}
 
 	/* returns the default tree selections choices */
+    @BodyParser.Of(BodyParser.Json.class)
 	public static Result defaultTree(){
 
-		return ok("tree is coming");
+        JsonNode json = request().body().asJson();
+        ObjectNode result = Json.newObject();
+        String name = json.findPath("name").textValue();
+        if(name == null) {
+            result.put("status", "KO");
+            result.put("message", "Missing parameter [name]");
+            return badRequest(result);
+        } else {
+            result.put("status", "OK");
+            result.put("message", "Hello " + name);
+            return ok(result);
+        }
 
 	}
 
